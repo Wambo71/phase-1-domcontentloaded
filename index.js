b761afa088fb6518f9053a62d8f414e831c82007
Your Name <your.email@example.com>
2025-06-19 10:38:18 +0300

    creates load

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+// Your code goes
+
+document.addEventListener('DOMContentLoaded',function () {
+   console.log('The DOM has loaded')
+
+   const paragraph = this.getElementById('text')
+   paragraph.textContent = 'This is really cool!'
+});
+
+
+ 
+
